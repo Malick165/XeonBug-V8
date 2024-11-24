22a0b466e6b2084c1c47fbb4e940d211d1402763
Malick165 <164951887+Malick165@users.noreply.github.com>
2024-11-24 12:23:42 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -14,7 +14,7 @@ global.ownernomer = "923444844060"
 global.ownername = "Qasim Ali"
 global.ytname = "YT: GlobalTechInfo"
 global.socialm = "GitHub: GlobalTechInfo"
-global.location = "Pakistan, Karachi, Karachi"
+global.location = "Africa, Tanzania, dar es salaam"
 
 global.ownernumber = '923444844060'  //creator number
 global.ownername = 'Qasim Ali' //owner name
